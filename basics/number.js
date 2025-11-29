c2ebd9fc8fcf108de56da835682b37374698be52
yemanealem <yemanea223@gmail.com>
2025-11-29 13:48:17 +0100

    feat:number

---
+++ basics/number.js
@@ -0,0 +1,10 @@
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+var float = 3.14;
+console.log(decimal);
+console.log(hex);
+console.log(binary);
+console.log(octal);
+console.log(float);
---
+++ basics/number.ts
@@ -0,0 +1,11 @@
+let decimal: number = 6;
+let hex: number = 0xf00d;      
+let binary: number = 0b1010;   
+let octal: number = 0o744;     
+let float: number = 3.14;    
+
+console.log(decimal);
+console.log(hex);
+console.log(binary);
+console.log(octal);
+console.log(float);
